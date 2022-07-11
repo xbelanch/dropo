319810e3f2cb47bfbb8031fd35953a08e0b377d3
Xavier Belanche <xbelanch@protonmail.com>
2022-07-11 19:20:56 +0200

    Take screenshots from a list of resources and activities from a course

--- dropo.rb
+++ dropo.rb
@@ -4,7 +4,8 @@ require "selenium-webdriver"
 require "json"
 
 # parse arguments
-# 
+# @TODO: Handle with error args 
+# @TODO: Add support for json configs
 username, password, _courses = ARGV
 courses = JSON.parse(_courses)
 
@@ -14,14 +15,15 @@ options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 options.add_argument('--window-size=1280,1024') # interact with campus in desktop layout
 driver = Selenium::WebDriver.for :chrome, service: service, options: options
+driver.manage.timeouts.implicit_wait = 500
 exception = Selenium::WebDriver::Error::NoSuchElementError
 wait = Selenium::WebDriver::Wait.new(timeout: 30, interval: 5, message: 'Timed out after 30 sec', ignore: exception)
 
 begin
   # Visit IOC site and login
   driver.get "https://ioc.xtec.cat"
-  puts driver.title
-  puts driver.current_url  
+  puts "[i]: #{driver.title}"
+  puts "[i]: #{driver.current_url}"  
   login_button = wait.until {
     el = driver.find_element(:xpath, '//*[@id="login-campus-large"]')
     el if el.displayed?
@@ -39,21 +41,21 @@ begin
   driver.find_element(id: 'submitbutton').click
 
   # Wait until main page is loaded (fucking courses list!)
-  my_campus = wait.until {
-    list_courses = "/html/body/div[3]/div[3]/div/div/section[1]/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/ul"
-    el = driver.find_element(:xpath, list_courses)
-    el if el.displayed?
-  }
+  # my_campus = wait.until {
+  #   list_courses = "/html/body/div[3]/div[3]/div/div/section[1]/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/ul"
+  #   el = driver.find_element(:xpath, list_courses)
+  #   el if el.displayed?
+  # }
   
   # Visit a list of owned courses and take a screenshot of them
-  for id in courses
-     driver.get "https://ioc.xtec.cat/campus/course/view.php?id=#{id}"
+  for id_course in courses
+     driver.get "https://ioc.xtec.cat/campus/course/view.php?id=#{id_course}"
      course = wait.until {
        el = driver.find_element(id: 'page-content')
        el if el.displayed?
      }
      title = driver.find_elements(tag_name: 'h1').first
-     puts title.text
+     puts "[i]: Course: #{title.text}"
 
      # We're in! take one screenshot to prove it!
      # First of all, we get the actual page dimensions using javascript
@@ -62,7 +64,39 @@ begin
      # resize windows depending on its full size
      driver.manage.window.resize_to(width+100, height+100)
      # take one full page screenshot
-     driver.save_screenshot("./screenshot_#{id}.png")
+     driver.save_screenshot("./screenshot_#{id_course}.png")
+
+     # time to go forward. What if we parse all the whole activities and resources
+     # from the section 0?
+     section0_title = driver.find_element(:xpath, '//*[@id="section-0"]/div/h3')
+     puts "[i]: Section title: #{section0_title.text}"
+     section0_contents = driver.find_elements(:xpath, '//*[@id="section-0"]/div/ul/li')
+     # Traverse all that bunch of crap, open it and take a picture
+
+     # store urls of the contents
+     url_pages = Array.new
+     puts "[i]: Table of contents of this section:"
+     puts "---------------------------------------"
+     for url in driver.find_elements(:xpath, '//*[@id="section-0"]/div/ul/li')
+       url_pages << url.find_element(tag_name: 'a').attribute('href')
+       puts "- #{url.text}"
+     end
+     puts "---------------------------------------"
+
+     # visit content page and take a picture
+     for url in url_pages
+       driver.get url
+       puts "[i]: Taking a picture of #{driver.title} page" 
+       wait.until {
+         el = driver.find_element(id: 'page-content')
+         el if el.displayed?
+       }
+       # @TODO: Too much hackish!
+       id_picture = driver.title.downcase.tr(" ","_").tr(
+"ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+"AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz")
+       driver.save_screenshot("./screenshot_#{id_course}_#{id_picture}.png")
+     end
   end
   
 ensure
